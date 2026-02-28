2739ce5732f342833e385f7c016c7be887a75ec2
nphayden1 <nphayden1@gmail.com>
2026-02-28 00:01:54 +0000

    Update app.py

--- usr/share/tv-remote/app.py
+++ usr/share/tv-remote/app.py
@@ -232,4 +232,4 @@ def run():
 
 
 if __name__ == '__main__':
-        socketio.run(app, host='0.0.0.0', port=5001) 
+        socketio.run(app, host='0.0.0.0', port=5001, allow_unsafe_werkzeug=True) 
